6b2170a70de824651c4e8ec63263b681e250a9ea
Shankar Unki <unki.shankar@gmail.com>
2024-01-11 23:48:52 +0530

    Redux changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { StoreModule, provideStore } from '@ngrx/store';
 import { counterReducer } from './store/counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideStore(), importProvidersFrom(StoreModule, StoreModule.forRoot({
     count: counterReducer
-  }))]
+  })), provideStoreDevtools({ maxAge: 25, logOnly: true })]
 };
